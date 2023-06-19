f92a6dd3dd47591ab8a172b95a64f5317fbce998
jjbenitez06 <jj.benitez.valencia@alumnos.upm.es>
2023-06-19 16:47:47 +0200

    Funciones

--- TRABAJO/Trabajo.c
+++ TRABAJO/Trabajo.c
@@ -14,6 +14,9 @@ typedef struct
 
 //Prototipos de funciones
 void identificarse();
+void imprimirDatosMaximoPorAnio(LineaFichero* lineas, int numLineas);
+void ordenarAlfabeticamentePorTecnologia(LineaFichero* lineas, int numLineas);
+void imprimirAlfabeticamentePorTecnologia(LineaFichero* lineas, int numLineas);
 
 
 int main(int argc, char *argv[]) 
@@ -105,4 +108,95 @@ void identificarse()
     {
         printf("Agotaste todos tus intentos. Identificación fallida.\n");
     }
+}
+
+void imprimirDatosMaximoPorAnio(LineaFichero* lineas, int numLineas) 
+{
+    FILE* archivo = fopen("datos_maximos_por_anio.txt", "w");
+    if (archivo == NULL) 
+    {
+        printf("Error al abrir el archivo para escribir.\n");
+        return;
+    }
+
+    for (int i = 0; i < numLineas; i++) 
+    {
+        double max2021 = lineas[i].valores2021[0];
+        double max2022 = lineas[i].valores2022[0];
+
+        for (int j = 1; j < 12; j++) 
+        {
+            if (lineas[i].valores2021[j] > max2021)
+                max2021 = lineas[i].valores2021[j];
+
+            if (lineas[i].valores2022[j] > max2022)
+                max2022 = lineas[i].valores2022[j];
+        }
+
+        fprintf(archivo, "%s - Máximo 2021: %lf, Máximo 2022: %lf\n", lineas[i].tecnologia, max2021, max2022);
+    }
+
+    fclose(archivo);
+    printf("Los datos máximos por año han sido volcados en el archivo 'datos_maximos_por_anio.txt'.\n");
+}
+
+void ordenarAlfabeticamentePorTecnologia(LineaFichero* lineas, int numLineas) 
+{
+    for (int i = 0; i < numLineas - 1; i++) 
+    {
+        for (int j = 0; j < numLineas - i - 1; j++) 
+        {
+            if (strcmp(lineas[j].tecnologia, lineas[j + 1].tecnologia) > 0) 
+            {
+                LineaFichero temp = lineas[j];
+                lineas[j] = lineas[j + 1];
+                lineas[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void imprimirAlfabeticamentePorTecnologia(LineaFichero* lineas, int numLineas) 
+{
+    ordenarAlfabeticamentePorTecnologia(lineas, numLineas);
+
+    FILE* archivo = fopen("orden_alfabetico_por_tecnologia.txt", "w");
+    if (archivo == NULL) 
+    {
+        printf("Error al abrir el archivo para escribir.\n");
+        return;
+    }
+
+    for (int i = 0; i < numLineas; i++) 
+    {
+        fprintf(archivo, "%s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+            lineas[i].tecnologia,
+            lineas[i].valores2021[0], 
+            lineas[i].valores2021[1],
+            lineas[i].valores2021[2],
+            lineas[i].valores2021[3],
+            lineas[i].valores2021[4],
+            lineas[i].valores2021[5],
+            lineas[i].valores2021[6],
+            lineas[i].valores2021[7],
+            lineas[i].valores2021[8],
+            lineas[i].valores2021[9],
+            lineas[i].valores2021[10],
+            lineas[i].valores2021[11],
+            lineas[i].valores2022[0], 
+            lineas[i].valores2022[1],
+            lineas[i].valores2022[2],
+            lineas[i].valores2022[3],
+            lineas[i].valores2022[4],
+            lineas[i].valores2022[5],
+            lineas[i].valores2022[6],
+            lineas[i].valores2022[7],
+            lineas[i].valores2022[8],
+            lineas[i].valores2022[9],
+            lineas[i].valores2022[10],
+            lineas[i].valores2022[11]);
+    }
+
+    fclose(archivo);
+    printf("Los datos ordenados alfabéticamente por tecnología han sido volcados en el archivo 'orden_alfabetico_por_tecnologia.txt'.\n");
 }
